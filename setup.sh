a5c91c2a4df20d3e2300554bc18b600197d696a1
siemonday <siemonday@gmail.com>
2013-11-28 17:27:06 +0800

    setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ DEST=$HOME
 
 ls -1 | grep '^_' | while read file; do
     # replace _ with .
-    link=$(expr substr $file 2 $(expr length $file))
+    link=${file:1}
     echo "$SOURCE/$file -> $DEST/.$link"
     ln -s $([ "$1" == '-f' ] && echo '-f') "$SOURCE/$file" "$DEST/.$link"
 done
